5aae5c841d8c16638e00d3e3e4be4c9a1e7f9b45
Jos van Olst <vanolstj@gmail.com>
2020-05-10 21:44:15 +1200

    Rearranged project structure so server and client are clearly seperated

--- server/server.js
+++ server/server.js
@@ -4,7 +4,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
-const sessions = require('express-session')
 
 
 // Local Dependancies
